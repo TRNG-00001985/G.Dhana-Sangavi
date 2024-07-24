8a7ef8be8be899532b45b75db10e79a3fca09377
GDhana_Sangavi <dhanasangavi2@gmail.com>
2024-07-24 13:43:21 +0530

    IndexOf.java

---
+++ IndexOf.java
@@ -0,0 +1,15 @@
+package Default;
+import java.util.Scanner;
+public class IndexOf {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String s1 = obj.nextLine(); 
+        System.out.println(s1.indexOf("R",4));
+		
+		
+
+	}
+
+}
