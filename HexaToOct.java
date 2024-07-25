5217dcbcf982bc6fb16ef7bea47f42f35b9a4607
GDhana_Sangavi <dhanasangavi2@gmail.com>
2024-07-25 09:42:32 +0530

    HexaToOct.java

---
+++ HexaToOct.java
@@ -0,0 +1,11 @@
+package Default;
+
+public class HexaToOct {
+
+	public static void main(String[] args) {
+		Integer i = Integer.parseInt("011");
+		System.out.println(Integer.toHexString(i));
+
+	}
+
+}
